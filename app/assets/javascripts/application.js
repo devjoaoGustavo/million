107b447807c7deb2703754993f5043e3436e6be9
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2018-08-28 00:05:10 -0300

    add materialize and start define layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery
 //= require_tree .
 //= require rails.validations
+//= require materialize.min
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
+
+$(".dropdown-trigger").dropdown();
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {});
+});

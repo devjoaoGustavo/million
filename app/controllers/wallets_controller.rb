6a69b9a533c25c26eaa59874978cc7f5c58a586e
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2019-07-02 05:32:21 -0300

    use transform on form

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -2,4 +2,29 @@ class WalletsController < ApplicationController
   def index
     @wallets = WalletDecorator.decorate_collection current_user.wallets
   end
+
+  def edit
+    @wallet = Wallet
+      .find(params[:id])
+      .then do |wallet|
+      WalletDecorator.decorate(wallet)
+    end
+  end
+
+  def update
+    Wallet.find(params[:id]).then do |wallet|
+      if wallet.update(update_params)
+        redirect_to user_wallets_path(current_user), notice: 'cateira atualizada'
+      else
+      end
+    end
+  end
+
+  private
+
+  def update_params
+    params
+      .require(:wallet)
+      .permits(:name)
+  end
 end
